ba9b20e1b31204af4da280244d718f8bf509f048
hihi4605 <christianmichel205@gmail.com>
2023-02-14 09:32:50 -0500

    2/14/23

--- mucrawler/src/main/java/edu/millersville/csci406/spring2023/CrawlerMain.java
+++ mucrawler/src/main/java/edu/millersville/csci406/spring2023/CrawlerMain.java
@@ -1,9 +1,37 @@
 package edu.millersville.csci406.spring2023;
 
 public class CrawlerMain {
-    PGCrawlingDataSource source = new PGCrawlingDataSource(null, null, null, null, null);
-    MyCrawlController controller = new MyCrawlController(source, 1000);
-    NetworkURLReader reader = new NetworkURLReader();
-    Thread one = new Thread(new CrawlWorkerThread(controller, reader));
 
-}
+    long crawlDelay = 10000;
+ 
+        PGCrawlingDataSource source = new PGCrawlingDataSource(null, null, null, null, null);
+        MyCrawlController controller = new MyCrawlController(source,crawlDelay);
+        NetworkURLReader reader = new NetworkURLReader();
+        Thread one = new Thread(new CrawlWorkerThread(controller, reader));
+        Thread two = new Thread(new CrawlWorkerThread(controller, reader));
+        Thread three = new Thread(new CrawlWorkerThread(controller, reader));
+        Thread four = new Thread(new CrawlWorkerThread(controller, reader));
+        Thread five = new Thread(new CrawlWorkerThread(controller, reader));
+        Thread six = new Thread(new CrawlWorkerThread(controller, reader));
+        Thread seven = new Thread(new CrawlWorkerThread(controller, reader));
+        Thread eight = new Thread(new CrawlWorkerThread(controller, reader));
+        Thread nine = new Thread(new CrawlWorkerThread(controller, reader));
+        Thread ten = new Thread(new CrawlWorkerThread(controller, reader));
+        one.start();
+        two.start();
+        three.start();
+        four.start();
+        five.start();
+        six.start();
+        seven.start();
+        eight.start();
+        nine.start();
+        ten.start();
+        one.join();
+        two.join();
+        three.join();
+        four.join();
+        
+    }
+   
+
